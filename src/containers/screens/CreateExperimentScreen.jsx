496044960fbb80ae796f8eba08f13f3f7d631b73
nkemcels <nkemcels@gmail.com>
2020-01-30 20:38:17 +0100

    adds confirmation action before a connection is detached

--- src/containers/screens/CreateExperimentScreen.jsx
+++ src/containers/screens/CreateExperimentScreen.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Button, Step, Icon } from "semantic-ui-react";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { showWarningNotification, showErrorNotification } from "../../components/utils/alerts";
+import { showWarningNotification, showErrorNotification, confirmationAlert } from "../../components/utils/alerts";
 import { connect } from "react-redux";
 import * as actions from "../../actions/componentActions";
 import {bindActionCreators} from "redux";
 import {jsPlumb} from "jsplumb"
+import { CONFIGURE_COMPONENT_ROUTE, CREATE_EXPERIMENT_ROUTE, CONFIGURE_EXPERIMENT_ROUTE } from "../../constants/app_routes";
 
 function mapStateToProps(state){
     return {
@@ -52,17 +53,54 @@ const ConfigComponentItem = (props)=>{
                 <h4>{props.name}</h4>
             </div>
             <div className="controls">
-                <Button negative onClick={()=>props.onDelete(props._id)}><Icon name="trash" /> Remove</Button>
+                <Button negative onClick={()=>props.onRemove(props._id)}><Icon name="trash" /> Remove</Button>
             </div>
         </div>
     )
 }
+
+const ConfigureComponentView = (props)=>(
+    <div className={"match-parent center-content "+(props.isActive?"":"hidden")}>
+        <div className="canvas-view">
+            <div className="header">  Component Canvas  </div>
+            <div className="body">
+                <div className="absolute-content">
+                    <Droppable droppableId="#canvas-droppable-view">
+                    {(provided, snapshot) => (
+                        <div
+                            ref={provided.innerRef}
+                            style={{ backgroundColor: snapshot.isDraggingOver && "#E3F2FD" }}
+                            className="match-parent"
+                        >
+                            {props.componentNodes.map(item=>(
+                                <ConfigComponentItem 
+                                    {...item} 
+                                    key={item._id} 
+                                    itemRef={props.nodeRefList[item._id]} 
+                                    onRemove={props.onRemoveCompConfig} />
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                    </Droppable>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+const ConfigureExperimentView = (props)=>(
+    <div className={"match-parent center-content "+(props.isActive?"":"hidden")}>
+        EXPERIMENT VIEW
+    </div>
+)
 
 class CreateExperimentScreen extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             componentNodes: [],
+            currentStep: "CONFIGURE_COMPONENT"
         }
         this.nodeRefs = {}
         this.jsPlumb = jsPlumb.getInstance({
@@ -71,12 +109,16 @@ class CreateExperimentScreen extends React.Component{
               stroke:"#BF360C"
             },
             ConnectionOverlays: [["Arrow", {location: 0.5, width:15, length:15}]],
-            Connector:[ "Flowchart", { curviness: 100 } ],
+            Connector:[ "Bezier", { curviness: 100 } ],
             Endpoint:[ "Rectangle", { radius:7 } ],
             EndpointStyle : { fill: "#567567"  },
             Anchor : [ "Continuous", {} ]
-          });
+        });
+        this.jsPlumb.bind("beforeDetach", ()=>{
+            return confirm("Detach this connection link?")
+        })
     }
+
     handleComponentDropped = (data)=>{
         // if(this.state.componentNodes.length === 2){
         //     showWarningNotification("You cannot add more than two components to the canvas");
@@ -96,16 +138,21 @@ class CreateExperimentScreen extends React.Component{
         }
     }
     handleRemoveComponentConfig = (compId)=>{
-        let domNode = ReactDOM.findDOMNode(this.nodeRefs[compId].current)
-        this.jsPlumb.remove(domNode)
-        delete this.nodeRefs[compId]
-        this.setState({componentNodes:this.state.componentNodes.filter(item=>item._id!==compId)}, ()=>{
-            this.jsPlumb.repaintEverything(false)
-        });
+        confirmationAlert("Are you sure you want to remove this component?", ()=>{
+            let domNode = ReactDOM.findDOMNode(this.nodeRefs[compId].current)
+            this.jsPlumb.remove(domNode)
+            delete this.nodeRefs[compId]
+            this.setState({componentNodes:this.state.componentNodes.filter(item=>item._id!==compId)}, ()=>{
+                this.jsPlumb.repaintEverything(false)
+            });
+        })
     }
-    handleMoveToCreateExp = ()=>{
+    handleMoveToConfigureExp = ()=>{
         let allConnections = this.jsPlumb.getAllConnections();
-
+        this.setState({currentStep: "CONFIGURE_EXPERIMENT"})
+    }
+    handleMoveToConfigureComp = ()=> {
+        this.setState({currentStep: "CONFIGURE_COMPONENT"})
     }
     _loadComponentDefinitions = ()=>{
         this.props.getComponentDefinition((success, error)=>{
@@ -148,13 +195,20 @@ class CreateExperimentScreen extends React.Component{
                     <div className="main-content">
                         <div className="stepper-container">
                             <Step.Group style={{width:"80%", padding:0}}>
-                                <Step active link>
+                                <Step  
+                                    active={this.state.currentStep === "CONFIGURE_COMPONENT"}
+                                    onClick={this.handleMoveToConfigureComp}
+                                >
                                     <Icon name='configure' />
                                     <Step.Content>
-                                        <Step.Title style={{fontSize:16,}}>Configure Component</Step.Title>
+                                        <Step.Title style={{fontSize:16,}}>
+                                            Configure Component
+                                        </Step.Title>
                                     </Step.Content>
                                 </Step>
-                                <Step link>
+                                <Step 
+                                    active={this.state.currentStep === "CONFIGURE_EXPERIMENT"}
+                                    onClick={this.handleMoveToConfigureExp}>
                                     <Icon name='lab' />
                                     <Step.Content>
                                         <Step.Title style={{fontSize:16}}>Configure Experiment</Step.Title>
@@ -164,34 +218,24 @@ class CreateExperimentScreen extends React.Component{
                         </div>
                         <div className="current-display center-content">
                             <div className="absolute-content">
-                                <div className="match-parent center-content">
-                                    <div className="canvas-view">
-                                        <div className="header">  Component Canvas  </div>
-                                        <div className="body">
-                                            <div className="absolute-content">
-                                            
-                                                <Droppable droppableId="#canvas-droppable-view">
-                                                {(provided, snapshot) => (
-                                                    <div
-                                                        ref={provided.innerRef}
-                                                        style={{ backgroundColor: snapshot.isDraggingOver && "#E3F2FD" }}
-                                                        className="match-parent"
-                                                    >
-                                                        {this.state.componentNodes.map(item=>(
-                                                            <ConfigComponentItem key={item._id} {...item} itemRef={this.nodeRefs[item._id]} onDelete={this.handleRemoveComponentConfig} />
-                                                        ))}
-                                                        {provided.placeholder}
-                                                    </div>
-                                                )}
-                                                </Droppable>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <ConfigureComponentView 
+                                    componentNodes={this.state.componentNodes}
+                                    onRemoveCompConfig={this.handleRemoveComponentConfig}
+                                    nodeRefList={this.nodeRefs}
+                                    isActive={this.state.currentStep === "CONFIGURE_COMPONENT"}
+                                /> 
+                                <ConfigureExperimentView 
+                                    isActive={this.state.currentStep === "CONFIGURE_EXPERIMENT"}
+                                />
                             </div>
                         </div>
                         <div className="content-footer">
-                            <Button size="big" primary onClick={this.handleMoveToCreateExp}>NEXT <Icon name="arrow right" /> </Button>
+                            {this.state.currentStep === "CONFIGURE_COMPONENT" &&
+                                <Button size="big" primary onClick={this.handleMoveToConfigureExp}>NEXT <Icon name="arrow right" /> </Button>
+                            }
+                            {this.state.currentStep === "CONFIGURE_EXPERIMENT" &&
+                                <Button size="big" primary onClick={this.handleMoveToConfigureComp}> <Icon name="arrow left" /> BACK </Button>
+                            }
                         </div>
                     </div>
                 </DragDropContext>
