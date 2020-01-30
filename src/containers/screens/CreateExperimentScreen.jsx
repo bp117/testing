66e8d0abf3bd72b7c213ce9a8328e98c3c982d07
nkemcels <nkemcels@gmail.com>
2020-01-30 12:06:37 +0100

    creates and assembles all components for the configure component screen

--- src/containers/screens/CreateExperimentScreen.jsx
+++ src/containers/screens/CreateExperimentScreen.jsx
@@ -1,31 +1,59 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Step, Icon } from "semantic-ui-react";
 import { Switch } from "react-router-dom";
+
+const PaletteMenuItem = (props)=>(
+    <div className="palette-item-container">
+        <div className="palette-item">
+            <h4>Item Name</h4>
+        </div>
+    </div>
+)
 
 export default class CreateExperimentScreen extends React.Component{
     render(){
         return (
             <div className="match-parent create-experiment-screen">
                 <div className="sidebar-palette">
-                    <div className="absolute-content">
-                        THIS IS MY SIDEBAR
+                    <div className="header">Components Palette</div>
+                    <div style={{flex:1, position:"relative"}}>
+                        <div className="absolute-content">
+                            <PaletteMenuItem />
+                            <PaletteMenuItem />
+                            <PaletteMenuItem />
+                            <PaletteMenuItem />
+                            <PaletteMenuItem />
+                        </div>
                     </div>
                 </div>
                 <div className="main-content">
                     <div className="stepper-container">
-                        HERE IS MY COOL AND GOOD LOOKING STEPPER
+                        <Step.Group style={{width:"80%", padding:0}}>
+                            <Step active link>
+                                <Icon name='configure' />
+                                <Step.Content>
+                                    <Step.Title style={{fontSize:16,}}>Configure Component</Step.Title>
+                                </Step.Content>
+                            </Step>
+                            <Step link>
+                                <Icon name='lab' />
+                                <Step.Content>
+                                    <Step.Title style={{fontSize:16}}>Configure Experiment</Step.Title>
+                                </Step.Content>
+                            </Step>
+                        </Step.Group>
                     </div>
                     <div className="current-display center-content">
                         <div className="absolute-content">
-                            <div className="configure-component-container">
+                            <div className="match-parent center-content">
                                 <div className="canvas-view">
-                                    HERE IS THE CANVAS
+                                    <div className="header">  Component Canvas  </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="content-footer">
-                        <Button size="big" primary>NEXT</Button>
+                        <Button size="big" primary>NEXT <Icon name="arrow right" /> </Button>
                     </div>
                 </div>
             </div>
